350a65cf4960cdee6e482d9942da74b343fe0e27
Drishtant singh sengar <118834647+0Drishtant0@users.noreply.github.com>
2025-12-17 01:29:35 +0530

    build: setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+        jest: true
+    },
+    extends: [
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    rules: {
+        'indent': ['error', 4],
+        'linebreak-style': 'off', // Allow both Windows (CRLF) and Unix (LF) line endings
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+        'no-console': 'off',
+        'comma-dangle': ['error', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+        'array-bracket-spacing': ['error', 'never'],
+        'max-len': ['warn', { 'code': 120, 'ignoreComments': true }]
+    }
+};
